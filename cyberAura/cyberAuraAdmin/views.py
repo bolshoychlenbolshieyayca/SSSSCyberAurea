ceac5b591942c153fab405a9e2c1308eaea93fc7
abdul <itsmymail05@mail.ru>
2026-02-15 10:27:08 +0300

    Alordonse

--- cyberAura/cyberAuraAdmin/views.py
+++ cyberAura/cyberAuraAdmin/views.py
@@ -3,18 +3,27 @@ from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
 from django.urls import reverse
 
+
 def index(request):
-    t = render_to_string('')
-    return HttpResponse(t)
+    # t = render_to_string('cyberAuraAdmin/index.html')
+    # return HttpResponse(t)
+    data = {'title': 'Главная страница'}
+    return render(request, 'cyberAuraAdmin/index.html', data)
+
+
+def about(request):
+    return render(request, 'cyberAuraAdmin/about.html', {'title': 'О сайте'})
 
 
 def categories(request, cat_id):
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
 
+
 def categories_by_slug(request, cat_slug):
     if request.POST:
         print(request.POST)
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+
 
 def archive(request, year):
     if year > 2023:
@@ -22,6 +31,7 @@ def archive(request, year):
         return HttpResponsePermanentRedirect(uri)
 
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
