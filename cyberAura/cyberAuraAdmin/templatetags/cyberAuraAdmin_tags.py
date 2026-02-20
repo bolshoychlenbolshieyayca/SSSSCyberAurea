b90310923c975cadf0e77dec121d15b633e8942c
abdul <itsmymail05@mail.ru>
2026-02-20 03:02:41 +0300

    -

--- cyberAura/cyberAuraAdmin/templatetags/cyberAuraAdmin_tags.py
+++ cyberAura/cyberAuraAdmin/templatetags/cyberAuraAdmin_tags.py
@@ -1,15 +1,11 @@
 from django import template
 from cyberAuraAdmin import views
+from cyberAuraAdmin.models import Category
 
 register = template.Library()
 
 
-@register.simple_tag(name='getcats')
-def get_categories():
-    return views.cats_db
-
-
 @register.inclusion_tag('cyberAuraAdmin/list_categories.html')
 def show_categories(cat_selected=0):
-    cats = views.cats_db
+    cats = Category.objects.all()
     return {'cats': cats, 'cat_selected': cat_selected} 
